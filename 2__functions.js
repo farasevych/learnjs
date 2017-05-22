9e5089f884586e4f17f046ff461b432cb55a2b5d
farasevych <farasevych@gmail.com>
2017-05-22 21:14:09 +0300

    function

--- 2__functions.js
+++ 2__functions.js
@@ -6,6 +6,9 @@ var str = "Donatas Banionis";
 str = str.replace ("s", "");
 console.log (str);
 
+
+
+// функція get_square буде повертати квадрат числа, тобто, число, помножене само на себе
 var get_square = function(num) {
 	return num * num;
 }
@@ -13,3 +16,16 @@ var get_square = function(num) {
 var a = 3;
 var b = get_square(a);
 console.log (b);
+
+
+
+// функція повертає строчку, загорнуту в заданий тег
+var wrap_in_tag = function(str, tagname) {
+	var wrapped = '<' + tagname + '>' + str + '</' + tagname + '>';
+	return wrapped;
+}
+
+var a = 'Hello, I love you, could you tell me your name';
+var b = 'span';
+var html = wrap_in_tag(a, b);
+console.log (html);
