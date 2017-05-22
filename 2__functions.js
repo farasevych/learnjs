5488bb3afecfff7191f7464f238cb5d39049b1e2
farasevych <farasevych@gmail.com>
2017-05-22 21:02:09 +0300

    self rite func

--- 2__functions.js
+++ 2__functions.js
@@ -1,3 +1,15 @@
-var user = "Zjenya"
+var user = "Zjenya";
 var greeting = 'Hello, Mr. ' + user;
 alert(greeting);
+
+var str = "Donatas Banionis";
+str = str.replace ("s", "");
+console.log (str);
+
+var get_square = function(num) {
+	return num * num;
+}
+
+var a = 3;
+var b = get_square(a);
+console.log (b);
